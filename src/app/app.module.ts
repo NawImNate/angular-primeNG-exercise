fb0d1c82680dd0b6d1afc73cd3e812baad0e50dd
nawimnate <jnhatch3@gmail.com>
2023-07-23 13:21:55 -0400

    various modules - primeng, forms, buttons, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ActivityFormComponent } from './activity-form/activity-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { EditActivityDialogComponent } from './edit-activity-dialog/edit-activity-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmptyTablePopupComponent } from './empty-table-popup/empty-table-popup.component';
+import { CalendarModule } from 'primeng/calendar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityFormComponent,
+    EditActivityDialogComponent,
+    EmptyTablePopupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatDialogModule,
+    TableModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    CalendarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
